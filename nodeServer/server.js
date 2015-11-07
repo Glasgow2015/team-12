75fb496b5775e5c1402b5b6a6062ebe4867d87e6
Robert Wilson <robert.wilson1717@gmail.com>
2015-11-07 14:57:55 +0000

    now serving your static files!

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('static'));
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
@@ -11,4 +13,3 @@ var server = app.listen(8080, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
-
