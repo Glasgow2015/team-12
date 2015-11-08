fa13ad8a7e1856e3aff93de4c70a45f3b947787d
gf38 <gf38@st-andrews.ac.uk>
2015-11-08 01:53:13 +0000

    Sms works

--- BeeTracker/app/src/main/java/forcebeewithyou/org/beetracker/MainActivity.java
+++ BeeTracker/app/src/main/java/forcebeewithyou/org/beetracker/MainActivity.java
@@ -1,13 +1,20 @@
 package forcebeewithyou.org.beetracker;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v4.net.ConnectivityManagerCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import java.sql.Connection;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -17,12 +24,13 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        NetSync sync = new NetSync();
+        //NetSync sync = new NetSync();
 
-        sync.start();
+        //sync.start();
 
         setContentView(R.layout.activity_main);
 
+        //Attaching event handlers to buttons
         Button inspectBtn = (Button) this.findViewById(R.id.inspect_new_btn);
 
         inspectBtn.setOnClickListener(new Button.OnClickListener(){
@@ -50,8 +58,34 @@ public class MainActivity extends ActionBarActivity {
                 startActivity(newFormInfo);
             }
         });
+
+        Button smsSyncBtn = (Button) this.findViewById(R.id.sync_test);
+
+        smsSyncBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SmsManager sm = SmsManager.getDefault();
+                String phoneNo = getString(R.string.sync_tel_num);
+                sm.sendTextMessage(phoneNo, null, "inspection new text", null, null);
+            }
+        });
+
+        //Checking network connectivity
+
+        ConnectivityManager cmc = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo ni = cmc.getActiveNetworkInfo();
+
+        if (ni != null && ni.isConnected()){
+            smsSyncBtn.setEnabled(false);
+        }
+        else{
+            smsSyncBtn.setEnabled(true);
+        }
     }
 
+    public void smsSync(){
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
