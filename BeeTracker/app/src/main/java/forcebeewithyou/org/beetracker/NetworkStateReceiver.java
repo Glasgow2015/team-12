0562332988d271a2ca92b0426da452722875cef4
gf38 <gf38@st-andrews.ac.uk>
2015-11-08 00:22:31 +0000

    HTTP requests in progress

---
+++ BeeTracker/app/src/main/java/forcebeewithyou/org/beetracker/NetworkStateReceiver.java
@@ -0,0 +1,30 @@
+package forcebeewithyou.org.beetracker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+/**
+ * Created by Gergely on 2015.11.08..
+ */
+public class NetworkStateReceiver extends BroadcastReceiver {
+    final String tag = NetworkStateReceiver.class.getName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(tag, "Network State Changing");
+
+        if (intent.getExtras() != null){
+            NetworkInfo ni = (NetworkInfo) intent.getExtras().get(ConnectivityManager.EXTRA_NETWORK_INFO);
+            if(ni!=null && ni.getState() == NetworkInfo.State.CONNECTED){
+                Log.i(tag, "Device is connected!");
+            }
+            else if(intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, Boolean.FALSE)){
+                Log.i(tag, "Device is disconnected!");
+            }
+        }
+    }
+}
