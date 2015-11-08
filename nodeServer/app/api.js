29046184061abacdbbdeeb37a707209f6ce84a79
Robert Wilson <robert.wilson1717@gmail.com>
2015-11-08 01:33:59 +0000

    that should work

--- nodeServer/app/api.js
+++ nodeServer/app/api.js
@@ -17,89 +17,91 @@ router.get('/', function(req, res) {
 router.get('/hive/:id', function(req, res) {
 
 
-pool.query('SELECT * FROM Hive WHERE IDHive = ? LIMIT 1',[req.params.id], function(err, rows, fields) {
-  if (err) console.log(err);
-  res.json(rows[0]);
-});
+  pool.query('SELECT * FROM Hive WHERE IDHive = ? LIMIT 1', [req.params.id], function(err, rows, fields) {
+    if (err) console.log(err);
+    res.json(rows[0]);
+  });
 
 
 });
 
-router.get('/hive', function(req,res){
+router.get('/hive', function(req, res) {
 
 
-pool.query('SELECT * FROM Hive', function(err, rows, fields) {
-  if (err) console.log(err  );
-  res.json(rows);
-});
+  pool.query('SELECT * FROM Hive', function(err, rows, fields) {
+    if (err) console.log(err);
+    res.json(rows);
+  });
 
 
 })
 
-router.post('/hive', function(req,res){
+router.post('/hive', function(req, res) {
 
 
 })
 
-router.post('/hive/:id', function(req,res){
+router.post('/hive/:id', function(req, res) {
   //TODO: Update Hive
 })
 
 
-router.get('/apiary', function(req,res){
+router.get('/apiary', function(req, res) {
 
 
-pool.query('SELECT * FROM ApiaryView', function(err, rows, fields) {
-  if (err) console.log(err  );
-  res.json(rows);
-});
-
-})
-
-
-router.get('/apiary/:id', function(req,res){
-
-pool.query('SELECT * FROM Apiary WHERE IDApiary = ? LIMIT 1',req.params.id, function(err, rows, fields) {
-  if (err) console.log(err  );
-  res.json(rows[0]);
-});
-
-router.post('/apiary', function(req,res){
-
-//var prepared = [req.body.name,req.body.lat,req.body.long,req.body.datecreated,harvmonth,environment,access];
-var prepared = ["Rob",1,1,1, "1,1,1,1,1,1,1,1,1,1,1,1",
-                                "1,1,1,1,1,1,1,1,1",
-                                "1,1,1,1,1,1,1,1"];
-pool.query('CALL CreateApiary(?,?,?,?,?,?,?)',prepared,function(err,rows,fields){
-  if(err) console.log(err);
-  res.status(200).end();
-});
-
-res.status(500).end();
-
-})
-
-router.get('/inspection', function(req,res){
-
-
-pool.query('SELECT * FROM Inspection', function(err, rows, fields) {
-  if (err) console.log(err  );
-  res.json(rows);
-});
+  pool.query('SELECT * FROM ApiaryView', function(err, rows, fields) {
+    if (err) console.log(err);
+    res.json(rows);
+  });
 
 })
 
 
-router.get('/inspection/:id', function(req,res){
+router.get('/apiary/:id', function(req, res) {
 
-pool.query('SELECT * FROM Apiary WHERE IDInspection = ? LIMIT 1',req.params.id, function(err, rows, fields) {
-  if (err) console.log(err  );
-  res.json(rows[0]);
+  pool.query('SELECT * FROM Apiary WHERE IDApiary = ? LIMIT 1', req.params.id, function(err, rows, fields) {
+    if (err) console.log(err);
+    res.json(rows[0]);
+  });
 });
 
+router.post('/apiary', function(req, res) {
+
+  //var prepared = [req.body.name,req.body.lat,req.body.long,req.body.datecreated,harvmonth,environment,access];
+  var prepared = ["Rob", 1, 1, 1, "1,1,1,1,1,1,1,1,1,1,1,1",
+    "1,1,1,1,1,1,1,1,1",
+    "1,1,1,1,1,1,1,1"
+  ];
+  pool.query('CALL CreateApiary(?,?,?,?,?,?,?)', prepared, function(err, rows, fields) {
+    if (err) console.log(err);
+    res.status(200).end();
+  });
+
+  res.status(500).end();
+
+})
+
+router.get('/inspection', function(req, res) {
+
+
+  pool.query('SELECT * FROM Inspection', function(err, rows, fields) {
+    if (err) console.log(err);
+    res.json(rows);
+  });
+
+})
+
+
+router.get('/inspection/:id', function(req, res) {
+
+  pool.query('SELECT * FROM Apiary WHERE IDInspection = ? LIMIT 1', req.params.id, function(err, rows, fields) {
+    if (err) console.log(err);
+    res.json(rows[0]);
+  });
+
 });
 
-router.post('/inspection',function(req,res){
+router.post('/inspection', function(req, res) {
   console.log(req.body);
   // pool.query('SELECT * FROM WeatherCondDict where NameType =  ? LIMIT 1',[req.body.weather],function(err, rows, fields){
   //   if(err) console.log(err);
@@ -129,59 +131,59 @@ router.post('/inspection',function(req,res){
   //
   // });
 
-  var prepared = [req.body.hiveid,req.body.date,req.body.weather,req.body.hivestate,req.body.colstr,req.body.temp,1,"1,1","1,1,1,1",1,4];
-           pool.query('CALL CreateInspection(?,?,?,?,?,?,?,?,?,?,?)',prepared,function(err,rows,fields){
-             if(err) console.log(err);
-             res.status(200).end();
-           });
+  var prepared = [req.body.hiveid, req.body.date, req.body.weather, req.body.hivestate, req.body.colstr, req.body.temp, 1, "1,1", "1,1,1,1", 1, 4];
+  pool.query('CALL CreateInspection(?,?,?,?,?,?,?,?,?,?,?)', prepared, function(err, rows, fields) {
+    if (err) console.log(err);
+    res.status(200).end();
+  });
   res.status(500).end();
 
 
 
 })
 
-router.get('/harvest', function(req,res){
+router.get('/harvest', function(req, res) {
 
 
-pool.query('SELECT * FROM Harvest', function(err, rows, fields) {
-  if (err) console.log(err  );
-  res.json(rows);
-});
-
-})
-
-
-router.get('/harvest/:id', function(req,res){
-
-pool.query('SELECT * FROM Harvest WHERE IDHarvest = ? LIMIT 1',req.params.id, function(err, rows, fields) {
-  if (err) console.log(err  );
-  res.json(rows[0]);
-});
-});
-
-
-router.get('/dictionary/:type',function(req,res){
-  if(req.params.type.indexOf('Dict', req.params.type.length - 'Dict'.length) !== -1){
-  pool.query('SELECT * FROM ?? ',req.params.type, function(err, rows, fields) {
-    if (err) console.log(err  );
+  pool.query('SELECT * FROM Harvest', function(err, rows, fields) {
+    if (err) console.log(err);
     res.json(rows);
   });
-}else{
-  res.json({});
-}
+
 })
 
-router.post('/user',function(req,res){
+
+router.get('/harvest/:id', function(req, res) {
+
+  pool.query('SELECT * FROM Harvest WHERE IDHarvest = ? LIMIT 1', req.params.id, function(err, rows, fields) {
+    if (err) console.log(err);
+    res.json(rows[0]);
+  });
+});
+
+
+router.get('/dictionary/:type', function(req, res) {
+  if (req.params.type.indexOf('Dict', req.params.type.length - 'Dict'.length) !== -1) {
+    pool.query('SELECT * FROM ?? ', req.params.type, function(err, rows, fields) {
+      if (err) console.log(err);
+      res.json(rows);
+    });
+  } else {
+    res.json({});
+  }
+})
+
+router.post('/user', function(req, res) {
 
   //TODO: Validate User params
   console.log(req.body);
-  pool.query('SELECT * FROM UserRoleDict where NameType =  ? LIMIT 1',[req.body.role],function(err, rows, fields){
-    if(err) console.log(err);
+  pool.query('SELECT * FROM UserRoleDict where NameType =  ? LIMIT 1', [req.body.role], function(err, rows, fields) {
+    if (err) console.log(err);
     console.log(rows);
     var hashedPass = crypto.createHash('md5').update(req.body.password);
-    var prepared = [req.body.name,req.body.username,hashedPass,rows[0].IDUserRole,req.body.email,req.body.phone];
-    pool.query('CALL CreateUser(?,?,?,?,?,?)',prepared,function(err,rows,fields){
-      if(err) console.log(err);
+    var prepared = [req.body.name, req.body.username, hashedPass, rows[0].IDUserRole, req.body.email, req.body.phone];
+    pool.query('CALL CreateUser(?,?,?,?,?,?)', prepared, function(err, rows, fields) {
+      if (err) console.log(err);
       res.status(200).end();
     });
   });
@@ -189,13 +191,13 @@ router.post('/user',function(req,res){
 
 });
 
-router.get('/user', function(req,res){
+router.get('/user', function(req, res) {
 
 
-pool.query('SELECT * FROM User', function(err, rows, fields) {
-  if (err) console.log(err  );
-  res.json(rows);
-});
+  pool.query('SELECT * FROM User', function(err, rows, fields) {
+    if (err) console.log(err);
+    res.json(rows);
+  });
 
 })
 
